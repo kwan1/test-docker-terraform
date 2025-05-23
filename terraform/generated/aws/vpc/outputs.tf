b8f34b4d828db6b01847c91c5415a4578ef824b1
Kwan Tan <tkwanxuan@gmail.com>
2025-05-23 14:21:34 +0800

    remove terraformer junk

--- terraform/generated/aws/vpc/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "aws_vpc_tfer--vpc-034847b011923fae4_id" {
-  value = "${aws_vpc.tfer--vpc-034847b011923fae4.id}"
-}
-
-output "aws_vpc_tfer--vpc-baf988dd_id" {
-  value = "${aws_vpc.tfer--vpc-baf988dd.id}"
-}
